a3032fda30b586e417920b84fb552f9f0b45eedf
Seb Cantin <sebastien.cantin@mail.mcgill.ca>
2023-09-26 00:43:45 +0000

    initial commit

---
+++ src/main/java/capstone/mfslbackend/MfslBackendApplication.java
@@ -0,0 +1,13 @@
+package capstone.mfslbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfslBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfslBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/mfslbackend/MfslBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.mfslbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfslBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
