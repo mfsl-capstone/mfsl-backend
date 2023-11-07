4a79f097e07aa4950b986c35e607a806ccd93423
abhijeetpraveen <abhijeetpraveen@gmail.com>
2023-11-06 19:07:38 -0500

    feat: CAP-19 - Repository fixes

--- src/main/java/capstone/mfslbackend/repository/UserRepository.java
+++ src/main/java/capstone/mfslbackend/repository/UserRepository.java
@@ -4,6 +4,6 @@ import capstone.mfslbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     User findUserByUsername(String username);
 }
