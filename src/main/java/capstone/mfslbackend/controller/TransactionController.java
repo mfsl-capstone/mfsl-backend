3d7671df61816bf35ef8538ad2d8ffe9840f5316
Seb <sebastien.cantin@mail.mcgill.ca>
2024-01-29 16:27:18 -0500

    Fixed checkstyle

--- src/main/java/capstone/mfslbackend/controller/TransactionController.java
+++ src/main/java/capstone/mfslbackend/controller/TransactionController.java
@@ -3,7 +3,12 @@ package capstone.mfslbackend.controller;
 import capstone.mfslbackend.model.Transaction;
 import capstone.mfslbackend.service.TransactionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
--- src/main/java/capstone/mfslbackend/model/Transaction.java
+++ src/main/java/capstone/mfslbackend/model/Transaction.java
@@ -4,7 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.NoArgsConstructor;
+
 
 @Getter
 @Setter
